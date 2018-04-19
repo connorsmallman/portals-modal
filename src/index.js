7ed88d86b89b47d54e6163cc7aedf28ee4efb3ce
Connor Smallman <connor.smallman@bedegaming.com>
2018-04-19 11:14:50 +0100

    intial commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const appRoot = document.getElementById('root');
+const modalRoot = document.getElementById('modal');
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement('div');
+    this.el.className = 'my-modal';
+  }
+
+  componentDidMount() {
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el,
+    );
+  }
+}
+
+class Parent extends React.Component {
+  state = { show: false };
+  openModal = this.openModal.bind(this);
+  closeModal = this.closeModal.bind(this);
+
+  closeModal() {
+    this.setState({ show: false });
+  }
+
+  openModal() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.openModal}>Open</button>
+        <div onClick={this.closeModal}>
+          <Modal>
+            {this.state.show ? <Child/> : null}
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Child() {
+  // The click event on this button will bubble up to parent,
+  // because there is no 'onClick' attribute defined
+  return (
+    <div>
+      This is my modal. There are many like it, but this one is mine. My modal is my best friend. It is my life. I must master it as I master my life. My modal, without me, is useless.
+      <button>Close</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Parent />, appRoot);
